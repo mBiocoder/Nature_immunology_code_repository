e797fb6edd43cae4b6e1b611419a45e680b4f47a
schaeuble <s.schaeuble@gmail.com>
2023-05-08 22:25:00 +0200

    Added glycolysis barcode plot to enrichment script.

--- CD8_bulk_RNAseq/bulk_enrichment.R
+++ CD8_bulk_RNAseq/bulk_enrichment.R
@@ -1,7 +1,7 @@
 #===================================================================================================================================#
 # Title: Perform enrichment analysis with bulk RNA seq data
 # Author: Sascha Schäuble
-# Figures: Figure 4A,H
+# Figures: Figure 4A,H, Supplementary Figure S12B 
 #===================================================================================================================================#
 
 
@@ -13,11 +13,16 @@
 
 library("tidyverse")
 library("magrittr")
+library("data.table")
 library("ggpubr")
 library("ggsci")
 library("clusterProfiler")
+library("fgsea")
+library("limma")
 library("org.Hs.eg.db")
-library("data.table")
+
+
+
 
 
 DATE_STR <- format(Sys.time(), "%y%m%d")
@@ -30,7 +35,7 @@ FN3 <- "revigo_ora_upGOBP_mitos_230418.tsv" # REVIGO reduction using 0.5 with
 SAVE_OUT <- F
 ADD_EPS2DUPSEXPR <- T # some log2fc have the same value - add a small eps to resolve
 REMOVE_DUPIDS <- T
-
+PVAL_CUT <- 0.05
 # ================================================================ #
 
 
@@ -64,6 +69,185 @@ add_eps2_dups <- function(df, my_seed = 1722) {
   return(df)
 }
 # ================================================================ #
+
+
+#### Description: gives back per factor the top DEGs
+#
+# IN:
+#    df with data and comp variable for comparisons
+#    criteria - which column should be arranged and the criteria?
+#    direction - asc or desc?
+#    topN - how many top N genes to pick (or fewer if there are less)?
+#    id - which gene id? default is "geneid" which is ensembl
+# OUT:
+#   df with top degs, unique rows for comp and geneid
+# PRE:
+#   df has to have "comp" and id variable
+#
+pickTopGenes <- function(df, criteria, direction = "desc", topN = NULL, id = "geneid") {
+  res <- tibble()
+  for (c in levels(df$comp)) {
+    tmp <- df %>% filter(comp == c) %>% 
+      dplyr::select(comp,id) %>% 
+      distinct()
+    
+    if (direction == "desc")
+      tmp %<>% arrange(desc(criteria))
+    else
+      tmp %<>% arrange(criteria)
+    
+    if (!is.null(topN))
+      tmp %<>% head(topN)
+    
+    res %<>% rbind(tmp)
+    
+  }
+  return(res)
+}
+# ================================================================ #
+
+#
+#### Description: simple helper
+#
+# IN:
+#    df - salt
+#    id - "symbol" - SYMBOL, ensembl for any other string
+# PRE:
+#   genename or geneid column in df
+#
+getTopSaltGenes <- function(df, term = "salt_signature", n = 100,
+                            id = "symbol", returnType = "list") {
+  if (id == "symbol") {
+    res <- tibble(signature = term,
+                  id = df$genename %>% head(n))
+  } else {
+    res <- tibble(signature = term,
+                  id = df$geneid %>% head(n))
+  }
+  if (returnType == "list") {
+    res.list <- list()
+    for (f in (res$signature %>% as_factor() %>% levels())) {
+      res.list[[f]] <- res %>% filter(signature == f) %>% pull(id)
+    }
+    res <- res.list
+  }
+  return(res)
+}
+# ================================================================ #
+
+#
+#### Description: produces barcode plots adapted from fgsea package
+#
+getMyBarcode <-
+  function(pathway,
+           stats,
+           gseaParam = 1,
+           ticksSize = 0.2,
+           scoreType = "std",
+           minGS = 1,
+           maxGS = length(stats) - 1,
+           exp = 1,
+           nPerm = 500,
+           n1 = NA,
+           n2 = NA,
+           title = NULL) {
+    
+    # stats
+    tmp.fgsea <- fgsea::fgsea(
+      pathways = pathway,
+      stats = stats,
+      minSize = minGS,
+      maxSize = maxGS,
+      scoreType = scoreType,
+      gseaParam = exp,
+      nproc = 4,
+      eps = 0
+    )
+    
+    # plot - enabling "positive" one tailed test
+    rnk <- rank(-stats)
+    ord <- order(rnk)
+    statsAdj <- stats[ord]
+    statsAdj <- sign(statsAdj) * (abs(statsAdj) ^ gseaParam)
+    statsAdj <- statsAdj / max(abs(statsAdj))
+    pathway <-
+      unname(as.vector(na.omit(match(
+        pathway[[1]], names(statsAdj)
+      ))))
+    pathway <- sort(pathway)
+    gseaRes <- calcGseaStat(
+      statsAdj,
+      selectedStats = pathway,
+      returnAllExtremes = TRUE,
+      scoreType = "pos"
+    )
+    bottoms <- gseaRes$bottoms
+    tops <- gseaRes$tops
+    n <- length(statsAdj)
+    xs <- as.vector(rbind(pathway - 1, pathway))
+    ys <- as.vector(rbind(bottoms, tops))
+    toPlot <- data.frame(x = c(0, xs, n + 1), y = c(0, ys, 0))
+    diff <- (max(tops) - min(bottoms)) / 8
+    x = y = NULL
+    g <- ggplot(toPlot, aes(x = x, y = y)) +
+      geom_hline(
+        yintercept = max(tops),
+        colour = "red",
+        linetype = "dashed"
+      ) + geom_hline(
+        yintercept = min(bottoms),
+        colour = "red",
+        linetype = "dashed"
+      ) +
+      geom_hline(yintercept = 0, colour = "black") + geom_line(color = "green") +
+      theme_pubr() +
+      ggtitle(title) +
+      geom_segment(
+        data = data.frame(x = pathway),
+        mapping = aes(
+          x = x,
+          y = -diff /
+            2,
+          xend = x,
+          yend = diff / 2
+        ),
+        size = ticksSize
+      ) +
+      theme(panel.border = element_blank(), panel.grid.minor = element_blank()) +
+      labs(x = "Rank of genes", y = "Running enrichment score")  +
+      geom_point(color = "green", size = 0.1) +
+      annotate(
+        geom = "text",
+        x = 0,
+        y = -0.05,
+        label = paste0(
+          "NES = ",
+          format(tmp.fgsea$NES, digits = 3),
+          ", p = ",
+          format(tmp.fgsea$pval, digits = 3)
+        ),
+        color = "black",
+        size = 4,
+        hjust = "left"
+      )
+    if (!(is.na(n1) | is.na(n2))) {
+      g <- g +
+        annotate(
+          geom = "text",
+          x = 0,
+          y = -0.1,
+          label = paste0("list(n[1]==", n1, ", n[2]==", n2, ")"),
+          color = "black",
+          size = 4,
+          hjust = "left",
+          parse = T
+        )
+    }
+    
+    return(g)
+  }
+# ================================================================ #
+
 # ================================================================ #
 
 
@@ -129,6 +313,19 @@ dat.DEG.down <-
     LFC_cd8_highsalt_vs_cd8_lowsalt,
     FDR_cd8_highsalt_vs_cd8_lowsalt
   ) %>% arrange(desc(LFC_cd8_highsalt_vs_cd8_lowsalt))
+
+# get genes for KEGG pathways
+dat.kegg <- limma::getGeneKEGGLinks(species="hsa")
+dat.kegg$Symbol <- mapIds(org.Hs.eg.db, dat.kegg$GeneID,
+                          column="SYMBOL", keytype="ENTREZID")
+dat.kegg %<>% filter(PathwayID == "hsa00010") %>% 
+  dplyr::rename(comp = PathwayID)
+dat.kegg$comp %<>% as_factor()
+dat.kegg %<>% left_join(dat.expr.stats %>% 
+                          dplyr::select(entrez,geneid, genename,
+                                        FDR_cd8_highsalt_vs_cd8_lowsalt,
+                                        LFC_cd8_highsalt_vs_cd8_lowsalt), 
+                        by = c("Symbol" = "genename") )
 
 # ====================================================== #
 
@@ -272,8 +469,45 @@ p.mito <- ggbarplot(dat.mito %>% arrange(qvalMinusLog10) %>%
 )
 p.mito <- p.mito + labs(y = expression(paste("-log"[10], "(q-value)")))
 
-p.mito %>% ggexport(filename = paste0("barplot_mito_", DATE_STR, ".pdf"),
-                    width = 8, height = 4) 
-
+if (SAVE_OUT) {
+  p.mito %>% ggexport(
+    filename = paste0("barplot_mito_", DATE_STR, ".pdf"),
+    width = 8,
+    height = 4
+  )
+}
 
 # ================================================================ #
+
+
+#### barcode | glycolysis, GSEA ####################################
+
+### KEGG
+# using fgsea
+dat.glycolysis <- pickTopGenes(df = dat.kegg %>%
+                                 filter(comp =="hsa00010" &
+                                          FDR_cd8_highsalt_vs_cd8_lowsalt <= PVAL_CUT) %>% 
+                                 distinct(),
+                               criteria = "LFC_cd8_highsalt_vs_cd8_lowsalt")
+n = (dat.glycolysis %>% dim())[1]
+p.glycolysis <- getMyBarcode(pathway = getTopSaltGenes(df = dat.glycolysis, n = n, id = "geneid"),
+                             stats = ( dat.expr.geneList.ensembl ),
+                             scoreType = "pos",
+                             maxGS = 4000,
+                             minGS = 10, nPerm = 1e4,
+                             title = "KEGG glycolysis (ID hsa00010)"
+                             
+)
+
+if (SAVE_OUT) {
+  p.glycolysis %>%
+    cowplot::save_plot(
+      filename = paste0("KEGGpwys/",
+                        "glycolysis_n",
+                        n,
+                        "_barcode_",
+                        DATE_STR,
+                        ".pdf"),
+      base_height = 6
+    )
+}
